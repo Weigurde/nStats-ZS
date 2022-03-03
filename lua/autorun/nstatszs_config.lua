9d7afbd49815003d615516ca587b796f9fadc327
nightrally <nightrallymain@gmail.com>
2022-03-03 20:03:53 +0000

    Bruh im an idiot x)

--- lua/autorun/nstatszs_config.lua
+++ lua/autorun/nstatszs_config.lua
@@ -1,4 +1,4 @@
-nStats.ZS = nStats.ZS or {} -- If you delete this, script go breaky breaky
+nStats = nStats or {} nStats.ZS = nStats.ZS or {} -- If you delete this, script go breaky breaky
 
 -- If you are using the old ZS builds from around 2014, disable this!
 nStats.ZS.NoxZS = true
