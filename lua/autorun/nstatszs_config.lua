95bde9a69c0230ca8871a2e9769a2a4c7c151db5
nightrally <nightrallymain@gmail.com>
2022-03-03 20:05:48 +0000

    new function printClear and custom meta functions

--- lua/autorun/nstatszs_config.lua
+++ lua/autorun/nstatszs_config.lua
@@ -1,4 +1,14 @@
-nStats = nStats or {} nStats.ZS = nStats.ZS or {} -- If you delete this, script go breaky breaky
+local meta = FindMetaTable("Player") nStats = nStats or {} nStats.ZS = nStats.ZS or {} -- If you delete this, script go breaky breaky
 
 -- If you are using the old ZS builds from around 2014, disable this!
 nStats.ZS.NoxZS = true
+
+----------------------
+-- CUSTOM FUNCTIONS --
+----------------------
+
+function printClear(text)
+	print("")
+	print(text)
+	print("")
+end
