a935a5be2e4352cde48cc468e9160f5b6fe703a3
nightrally <nightrallymain@gmail.com>
2022-03-03 18:37:46 +0000

    Config + stats stuff

---
+++ lua/autorun/client/cl_stats.lua
@@ -0,0 +1,115 @@
+function nStatsClient(plyr)
+    local s = BetterScreenScale()
+
+    -- fix nil values if the plyr argument is missing
+    if not plyr then
+        plyr = LocalPlayer()
+    elseif not plyr:IsPlayer() then
+        plyr = LocalPlayer()
+    end
+    
+    printClear("[nStats] Currently viewing: "..tostring(plyr:Name()))
+
+    local frame = vgui.Create("DFrame")
+    frame:SetSize(500 * s, 500 * s)
+    frame:SetTitle("nStats-ZS | Personal Statistics")
+    frame:SetDraggable(false)
+    frame:Center()
+    frame:MakePopup()
+
+    local sheet = vgui.Create( "DPropertySheet", frame )
+    sheet:Dock( FILL )
+
+    local panel1 = vgui.Create( "DPanel", sheet )
+    sheet:AddSheet( "Human Stats", panel1, "icon16/user.png" )
+
+    local panel2 = vgui.Create( "DPanel", sheet )
+    sheet:AddSheet( "Zombie Stats", panel2, "icon16/user_red.png" )
+
+    local panel3 = vgui.Create( "DPanel", sheet )
+    sheet:AddSheet( "ZS Stats", panel3, "icon16/user_orange.png" )
+
+    local panel4 = vgui.Create( "DPanel", sheet )
+    sheet:AddSheet( "Player List", panel4, "icon16/group.png" )
+
+    local panel5 = vgui.Create( "DPanel", sheet )
+    sheet:AddSheet( "Admin Panel", panel5, "icon16/shield.png" )
+
+    local wide = frame:GetWide()
+    local tall = frame:GetTall()
+
+    local list1 = vgui.Create("DPanelList", panel1) list1:EnableVerticalScrollbar() list1:EnableHorizontal(false) list1:SetSize(wide - 12, tall - 12) list1:SetPos(0, 0) list1:SetPadding(8) list1:SetSpacing(16)
+    local list2 = vgui.Create("DPanelList", panel2) list2:EnableVerticalScrollbar() list2:EnableHorizontal(false) list2:SetSize(wide - 12, tall - 12) list2:SetPos(0, 0) list2:SetPadding(8) list2:SetSpacing(16)
+    local list3 = vgui.Create("DPanelList", panel3) list3:EnableVerticalScrollbar() list3:EnableHorizontal(false) list3:SetSize(wide - 12, tall - 12) list3:SetPos(0, 0) list3:SetPadding(8) list3:SetSpacing(16)
+    local list4 = vgui.Create("DPanelList", panel4) list4:EnableVerticalScrollbar() list4:EnableHorizontal(false) list4:SetSize(wide - 12, tall - 70) list4:SetPos(0, 0)
+    -----------------
+    -- HUMAN STATS --
+    -----------------
+
+    local function HumanStat(name, id) local check = vgui.Create("DLabel", frame) check:SetText(name..": "..tostring(string.CommaSeparate(math.floor(plyr:GetNWInt(id))))) check:SetFont("ZSHUDFontSmaller") check:SizeToContents() list1:AddItem(check) end
+    local check = vgui.Create("DLabel", frame) check:SetText(plyr:Name().."'s Stats ") check:SetFont("ZSHUDFontSmall") check:SizeToContents() check:SetTextColor(COLOR_CYAN) list1:AddItem(check)
+
+    HumanStat("Zombies Killed", "HStats.ZombiesKilled")
+    HumanStat("Zombies Killed Assists", "HStats.ZombiesKilledAssists")
+    HumanStat("Zombies Killed Headshots", "HStats.ZombiesKilledHeadshot")
+    HumanStat("Crows Killed", "HStats.CrowsKilled")
+    HumanStat("Barricade Repair Points", "HStats.BarricadeRepairPoints")
+    HumanStat("Damage To Zombies", "HStats.DamageToZombies")
+
+    ------------------
+    -- ZOMBIE STATS --
+    ------------------
+
+    local function ZombieStat(name, id) local check = vgui.Create("DLabel", frame) check:SetText(name..": "..tostring(string.CommaSeparate(math.floor(plyr:GetNWInt(id))))) check:SetFont("ZSHUDFontSmaller") check:SizeToContents() list2:AddItem(check) end
+    local check = vgui.Create("DLabel", frame) check:SetText(plyr:Name().."'s Stats ") check:SetFont("ZSHUDFontSmall") check:SizeToContents() check:SetTextColor(COLOR_CYAN) list2:AddItem(check)
+
+    ZombieStat("Humans Killed", "ZStats.HumansKilled")
+    ZombieStat("Barricade Damage", "ZStats.BarricadeDamage")
+    ZombieStat("Damage To Humans", "ZStats.DamageToHumans")
+
+    --------------
+    -- ZS STATS --
+    --------------
+
+    local check = vgui.Create("DLabel", frame) check:SetText(plyr:Name().."'s Stats ") check:SetFont("ZSHUDFontSmall") check:SizeToContents() check:SetTextColor(COLOR_CYAN) list3:AddItem(check)
+
+    local check = vgui.Create("DLabel", frame)
+    check:SetText("Level: "..plyr:GetZSLevel())
+    check:SetFont("ZSHUDFontSmaller")
+    check:SizeToContents()
+    list3:AddItem(check)
+
+    --[[local check = vgui.Create("DLabel", frame)
+    check:SetText("Remort: "..string.CommaSeparate(plyr:GetZSRemortLevel()))
+    check:SetFont("ZSHUDFontSmaller")
+    check:SizeToContents()
+    list3:AddItem(check)]]
+
+    local check = vgui.Create("DLabel", frame)
+    check:SetText("XP: "..string.CommaSeparate(plyr:GetZSXP()).." / "..string.CommaSeparate(GAMEMODE:XPForLevel(plyr:GetZSLevel() + 1)))
+    check:SetFont("ZSHUDFontSmaller")
+    check:SizeToContents()
+    list3:AddItem(check)
+
+    -----------------
+    -- PLAYER LIST --
+    -----------------
+
+    for _, v in pairs(player.GetHumans()) do
+        local button = vgui.Create("DButton", frame)
+        button:SetText(v:Name())
+        button:SetFont("ZSHUDFontSmall")
+        button:SizeToContents()
+        button:Dock(TOP)
+        button.DoClick = function()
+            Derma_Query("View "..v:Name().."'s personal statistics?", "", "Yes", function() frame:Close() nStatsClient(v) end, "No")
+        end
+        list4:AddItem(button)
+    end
+end
+
+net.Receive("ShowStatsRequest", function()
+    local plyr = net.ReadEntity()
+    
+    nStatsClient(plyr)
+end)
---
+++ lua/autorun/server/sv_stats.lua
@@ -0,0 +1,120 @@
+local meta = FindMetaTable("Player")
+
+function meta:UpdateStat(id, num)
+    self:SetNWInt(id, self:GetNWInt(id) + (num or 1))
+end
+
+function SaveStatistics(pl)
+    if not IsValid(pl) or pl:IsBot() then return end
+
+    local function Stat(name)
+        pl:SetPData(name, pl:GetNWInt(name))
+    end
+
+    Stat("HStats.ZombiesKilled")
+    Stat("HStats.ZombiesKilledAssists")
+    Stat("HStats.ZombiesKilledHeadshot")
+    Stat("HStats.CrowsKilled")
+    Stat("HStats.BarricadeRepairPoints")
+    Stat("HStats.DamageToZombies")
+
+    Stat("ZStats.HumansKilled")
+    Stat("ZStats.BarricadeDamage")
+    Stat("ZStats.DamageToHumans")
+end
+
+function LoadStatistics(pl)
+    if not IsValid(pl) or pl:IsBot() then return end
+
+    local function Stat(name)
+        pl:SetNWInt(name, pl:GetPData(name, 0))
+    end
+
+    Stat("HStats.ZombiesKilled")
+    Stat("HStats.ZombiesKilledAssists")
+    Stat("HStats.ZombiesKilledHeadshot")
+    Stat("HStats.CrowsKilled")
+    Stat("HStats.BarricadeRepairPoints")
+    Stat("HStats.DamageToZombies")
+
+    Stat("ZStats.HumansKilled")
+    Stat("ZStats.BarricadeDamage")
+    Stat("ZStats.DamageToHumans")
+end
+
+
+------------------
+-- SAVING STATS --
+------------------
+
+hook.Add("PlayerAuthed", "Stats.PlayerAuthed", LoadStatistics)
+hook.Add("PlayerInitialSpawnRound", "Stats.PlayerInitialSpawnRound", LoadStatistics)
+hook.Add("PlayerDisconnected", "Stats.PlayerDisconnected", SaveStatistics)
+
+hook.Add("EndRound", "Stats.EndRound", function(winner)
+    for _, pl in pairs(player.GetHumans()) do
+        SaveStatistics(pl)
+	end	
+end)
+
+-----------------
+-- HUMAN STATS --
+-----------------
+
+hook.Add("HumanKilledZombie", "Stats.HumanKilledZombie", function(pl, attacker, inflictor, dmginfo, headshot, suicide)
+	if (pl:GetZombieClassTable().Points or 0) == 0 or GAMEMODE.RoundEnded then
+        if pl:GetZombieClassTable().Name == "Crow" then
+            attacker:UpdateStat("HStats.CrowsKilled")
+        end
+        return
+    end
+
+	attacker:UpdateStat("HStats.ZombiesKilled")
+    
+    if pl:WasHitInHead() then
+        attacker:UpdateStat("HStats.ZombiesKilledHeadshot")
+    end
+end)
+
+hook.Add("PostHumanKilledZombie", "Stats.PostHumanKilledZombie", function(pl, attacker, inflictor, dmginfo, assistpl, assistamount, headshot)
+    if IsValid(assistpl) then
+	    assistpl:UpdateStat("HStats.ZombiesKilledAssists")
+    end
+end)
+
+hook.Add("PlayerRepairedObject", "Stats.PlayerRepairedObject", function(pl, other, health, wep)
+	if GAMEMODE:GetWave() == 0 or health <= 0 then return end
+
+	pl:UpdateStat("HStats.BarricadeRepairPoints", health)
+end)
+
+------------------
+-- ZOMBIE STATS --
+------------------
+
+hook.Add("ZombieKilledHuman", "Stats.ZombieKilledHuman", function(pl, attacker, inflictor, dmginfo, headshot, suicide)
+	if GAMEMODE.RoundEnded then return end
+
+	attacker:UpdateStat("ZStats.HumansKilled")
+end)
+
+hook.Add("OnNailDamaged", "ZStats.OnNailDamaged", function(attacker, inflictor, damage, dmginfo)
+    attacker:UpdateStat("ZStats.BarricadeDamage", damage)
+end)
+
+--------------------------
+-- ZOMBIE & HUMAN STATS --
+--------------------------
+
+hook.Add("PostEntityTakeDamage", "Stats.PostEntityTakeDamage", function(ent, dmgInfo, took)
+    local attacker = dmgInfo:GetAttacker()
+    local damage = dmgInfo:GetDamage()
+
+    if not ent:IsPlayer() or not attacker:IsPlayer() or GAMEMODE.RoundEnded then return end
+
+    if ent:Team() == TEAM_UNDEAD then
+        attacker:UpdateStat("HStats.DamageToZombies", damage)
+    elseif ent:Team() == TEAM_HUMAN then
+        attacker:UpdateStat("ZStats.DamageToHumans", damage)
+    end
+end)
